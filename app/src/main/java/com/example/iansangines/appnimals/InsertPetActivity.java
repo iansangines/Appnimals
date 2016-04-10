2d7eb28c7b36c5005275cfd9a6693b3c248c8457
ian <iansangines@gmail.com>
2016-04-10 21:20:22 +0200

    Implement spinner

--- app/src/main/java/com/example/iansangines/appnimals/InsertPetActivity.java
+++ app/src/main/java/com/example/iansangines/appnimals/InsertPetActivity.java
@@ -1,17 +1,10 @@
 package com.example.iansangines.appnimals;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.ExpandableListAdapter;
-import android.widget.ListView;
-import android.widget.ExpandableListView;
-import android.widget.SimpleExpandableListAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class InsertPetActivity extends AppCompatActivity {
@@ -24,6 +17,10 @@ public class InsertPetActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Spinner petTypes = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.especies,android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        petTypes.setAdapter(adapter);
     }
 
 }
