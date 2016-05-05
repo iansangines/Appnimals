c64dd274cbf893a90329ac288079d35dc0f3f22b
ian <iansangines@gmail.com>
2016-05-05 18:40:25 +0200

    Create ProfileActivity

---
+++ app/src/main/java/com/example/iansangines/appnimals/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.iansangines.appnimals;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
