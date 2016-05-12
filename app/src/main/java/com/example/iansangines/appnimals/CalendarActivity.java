d066a90c014792e87f3670d950deb5729a8b2909
ian <iansangines@gmail.com>
2016-05-12 18:14:59 +0200

    Add calendar

---
+++ app/src/main/java/com/example/iansangines/appnimals/CalendarActivity.java
@@ -0,0 +1,14 @@
+package com.example.iansangines.appnimals;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CalendarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+
+}
